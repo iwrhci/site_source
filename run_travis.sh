9dca55a44d7efb517989b2922a600dfeb0607b84
Francesco Biscani <bluescarni@gmail.com>
2016-09-08 11:20:10 +0200

    Various updates.

--- run_travis.sh
+++ run_travis.sh
@@ -5,12 +5,20 @@ set -e
 # Echo each command
 set -x
 
+# Install jinja2.
 pip install jinja2
+
+# Render the website.
 python render.py
+
+# Setup git infor.
 git config --global push.default simple
 git config --global user.name "Travis CI"
 git config --global user.email "bluescarni@gmail.com"
+
+# Now we will clone the website's repo, update it and push the new version.
 cd ..
+# Don't echo commands on the git clone, in order to avoid printing out sensitive info.
 set +x
 git clone "https://${GH_TOKEN}@github.com/iwrhci/iwrhci.github.io.git" iwrhci.github.io -q
 set -x
@@ -21,13 +29,4 @@ git add .
 # We assume here that a failure in commit means that there's nothing
 # to commit.
 git commit -m "Update website, commit ${TRAVIS_COMMIT} [skip ci]." || exit 0
-PUSH_COUNTER=0
-until git push -q
-do
-    git pull -q
-    PUSH_COUNTER=$((PUSH_COUNTER + 1))
-    if [ "$PUSH_COUNTER" -gt 3 ]; then
-        echo "Push failed, aborting."
-        exit 1
-    fi
-done
+git push -q
